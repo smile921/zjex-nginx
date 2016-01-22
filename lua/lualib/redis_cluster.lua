86603687127c7adbf3fa743306a087319c2ad4e2
Frère Jacques <zyb@zjex.com>
2016-01-22 17:10:20 +0800

    redis_cluster connect and redis  pool

---
+++ lua/lualib/redis_cluster.lua
@@ -0,0 +1,169 @@
+local redis = require ("resty.redis")
+local stringy = require "stringy" 
+local constants = require "constants" 
+local cfg   = require ("init_cfg")
+
+
+local _M = {}
+    _M.prefix = "zjex.session.id"
+local function isClusterChanged(ok,err)
+    if not ok and err then
+       local got = string.find(err,"MOVED") 
+       local errs = stringy.split(err," ")
+       local infos = errs[3]
+       if infos then
+          ngx.log(ngx.WARN,"<br> redis moved to "..(infos or "nil ").." <br>")
+          --ngx.say(iipp)
+          local ip_port = stringy.split(infos,":")
+          local ip = ip_port[1]
+          local port = ip_port[2] 
+          return true,ip,port
+       end
+    end
+    return false,nil,nil
+end
+
+ 
+
+function redis_pool_get_redis_conf(redis_name)
+    local redis_config = cfg;    
+    local redis_host = "127.0.0.1"
+    local redis_port = 6379
+    local redis_timeout = 10000
+    local redis_poolsize= 2000
+    if redis_config and type(redis_config.host) == "string" then
+        redis_host = redis_config.host
+    end
+    
+    if redis_config and type(redis_config.port) == "number" then
+        redis_port = redis_config.port
+    end
+
+    if redis_config and type(redis_config.timeout) == "number" then
+        redis_timeout= redis_config.timeout
+    end
+    
+    if redis_config and type(redis_config.poolsize) == "number" then
+        redis_poolsize= redis_config.poolsize
+    end
+    ngx.log(ngx.INFO,redis_host..":"..tostring(redis_port).."  "..tostring(redis_timeout).."  "..tostring(redis_poolsize))
+    return redis_host,redis_port,redis_timeout,redis_poolsize
+end
+
+function redis_pool_get_redis_conn(red_name) 	
+    local app_name = ngx.ctx.APP_NAME
+    local redis_name= red_name or 'redis'
+    local pool_name= app_name..redis_name
+    local red_pool = ngx.ctx[redis_pool]
+    if red_pool and red_pool[pool_name] then
+         return  red_pool[pool_name]
+    end
+    if not red_pool then
+        red_pool = {}
+    end 
+    local red = Redis:new()
+    local redis_host,redis_port,redis_timeout,redis_poolsize= redis_pool_get_redis_conf(redis_name)
+    local ok, err = red:connect(redis_host,redis_port)
+    if not ok then
+        ngx.log(ngx.ERR,"failed to connect: "..err)
+        return nil, err
+    end
+    ngx.log(ngx.INFO,"connect redis completed!")
+    red:set_timeout(redis_timeout) 
+    red_pool[pool_name] = red
+    ngx.ctx[redis_pool] = red_pool 
+    return  red  -- ngx.ctx[redis_pool]
+end
+
+function redis_pool_close(red_name)
+    local app_name = ngx.ctx.APP_NAME
+    local redis_name= red_name or 'redis'
+    local pool_name= app_name..redis_name
+    local red_pool = ngx.ctx[redis_pool]
+    if red_pool and red_pool[pool_name] then
+         local redis_host,redis_port,redis_timeout,redis_poolsize= redis_pool_get_redis_conf(redis_name)
+	 local red = red_pool[pool_name] 
+         red:set_keepalive(redis_timeout, redis_poolsize)
+         -- ngx.ctx[redis_pool] = nil
+         red_pool[pool_name] = nil
+     end
+ end
+
+-- 关闭或释放连接
+-- local function close_redis(red)  
+--   if not red then  
+--     return;  
+--   end  
+--   --释放连接(连接池实现)  
+--   local pool_max_idle_time = 10000 --毫秒  
+--   local pool_size = 100 --连接池大小  
+--   local ok, err = red:set_keepalive(pool_max_idle_time, pool_size)  
+--   if not ok then  
+--     ngx.log(ngx.DEBUG,"set keepalive error : " .. err)
+--     return ok,err  
+--   end
+--   return true,nil   
+-- end
+
+ 
+------------------------------------------------------
+--集群重连一次
+local function retryHset(red,keyname,field,value)
+      local ok,err = red:hset(keyname,field,value)
+      local flag,ip,port = isClusterChanged(ok,err)
+      if flag then
+         --重连，之后再次调用
+         red:connect(ip,port);
+         ngx.log(ngx.INFO,"<br>reconnect to ip="..(ip or "nil ip").."   port = "..(port or "nil port").."<br>")
+         ok,err = red:hset(keyname,field,value)
+      end
+      return ok,err
+end
+-- 
+local function retryHget(red,keyname,field)
+      local ok,err = red:hget(keyname,field)
+      local flag,ip,port = isClusterChanged(ok,err)
+      if flag then
+         --重连，之后再次调用
+         red:connect(ip,port);
+         set_keepalive_mod(red)
+         ngx.log(ngx.INFO,"<br>reconnect to ip="..(ip or "nil ip").."   port = "..(port or "nil port").."<br>")
+         ok,err = red:hget(keyname,field)
+      end
+      return ok,err
+end
+
+
+
+function _M.setSession(session_id,key,value)
+  
+  local red = redis_pool_get_redis_conn()  
+  local redis_key = _M.prefix..session_id
+  value  = json.encode(value);
+  ok, err = retryHset(red,redis_key ,key , value)
+  if not ok then
+    return
+  end
+  red:expire(redis_key,cfg.expire_timeout_second) --1  
+  local ok , err = red:set_keepalive(0,100)
+  return ok,err; 
+end
+
+function _M.getSession(session_id,key)
+    
+  local value = nil  
+  local red = redis_pool_get_redis_conn()
+  local redis_key = _M.prefix..session_id 
+  local res, err = retryHget(red,redis_key,key )
+  value = res
+  if value and tostring(value) == "userdata: NULL" then
+    value = nil
+  else
+    red:expire(redis_key,cfg.expire_timeout_second) --1000 - 1sec
+    ngx.log(ngx.DEBUG,"  redis:expire("..redis_key..","..init_cfg.expire_timeout_second.." second)")
+  end
+  red:set_keepalive(0,100)
+  return value 
+end
+--################################################################################
+return _M;
--- lua/redis_test.lua
+++ lua/redis_test.lua
@@ -1,33 +1,12 @@
 local init_cfg = require("init_cfg")
 local json     = require ("cjson")
 local redis = require ("resty.redis")
-  local json_parser = require "json_parser"
-  local oauth2 = require "oauth2"  
-  local constants = require "constants"
-  
-  local request_method = ngx.var.request_method;
-  local args=nil;   
-  ----获取参数的值
-  if "GET" == request_method then
-     args = ngx.req.get_uri_args();
-  elseif "POST" == request_method then
-     ngx.req.read_body();
-     args = ngx.req.get_post_args()
-  end
-  local conf = {};
-  args.client_id= init_cfg.client_id;
-  args.client_secret = init_cfg.client_secret;
-  args.redirect_uri = init_cfg.redirect_uri;
-  conf.args = args;
-  conf.grant_type = constants.GRANT_TYPE.GRANT_CLIENT_CREDENTIALS;
-  local a,b,c = oauth2.get_token_by_type(conf,conf.grant_type)
-  if a then
-    ngx.say("access_token : " .. a.access_token)
-    if c == 2 then
-       ngx.say("refresh_token : " .. a.refresh_token)
-    end
-  end
-  ngx.say("b : " .. type(b))
+local redis_cluster = require "redis_cluster";
+
+redis_cluster.setSession("abced","hello");
+ngx.say("<br" .. (redis_cluster.getSession("abced") or "error ") .."<br>")
+
+ 
   
 
   ngx.say("<br><br><br><br><br><br><br><br><br><br><br><br>");
